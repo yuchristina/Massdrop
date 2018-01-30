554ec95496bf15bfcd603c7a3a69b9d506fe8455
Christina Yu <30699619+yuchristina@users.noreply.github.com>
2018-01-29 17:57:43 -0800

    Implemented Kue

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,31 @@
-const express 		= require('express');
-const path 			= require('path');
-const bodyParser 	= require('body-parser');
+const express 	 = require('express');
+const path 			  = require('path');
+const bodyParser = require('body-parser');
 const cors 			= require('cors');
-const request 		= require('request');
+const request 	= require('request');
 const normalizeUrl 	= require('normalize-url');
 const escape 		= require("html-escape");
 const decode 		= require('unescape');
-const database 		= require('../Database/index.js');
+const database 	= require('../Database/index.js');
 const redis 		= require('redis');
+const kue       = require('kue');
 
 const app = express();
+
+//Create a job queue
+const queue = kue.createQueue({
+    prefix: 'q',
+    redis: {
+        createClientFactory: function () {
+            let client = redis.createClient({
+                port: process.env.REDIS_PORT,
+            });
+            return client;
+        }
+    }
+});
+
+queue.setMaxListeners(1000);
 
 app.use(express.static(__dirname + '/../Client'));
 
@@ -21,43 +37,60 @@ app.listen(3000, () => {
   console.log('listening on port 3000!');
 });
 
-//Create a Redis client object for use as cache on default port 6379
-const REDIS_PORT = process.env.REDIS_PORT;
-const client = redis.createClient(REDIS_PORT);
-client.on("error", (err) => {
-    console.log("Error:" + err);
-});
- 
 
 //Handles get requests from client with URL submission
 app.get('/getId', (req, res) => {
   const url = req.query.url;
-  //Enters URL into database
-  database.findID(url, (err, data) => {
-    if(err) {
-      res.sendStatus(404).end();
-    } else {
-      //If no error in entering the URL into the database, request for the HTML of the page
-      //using the normalized version of the URL
-      request(normalizeUrl(url), (error, response, body) => {
-      	if(error) {
-      	  //If error in fetching HTML from normalized URL, send response "Not Available"
-      	  res.status(200).send(' Not Available (The URL you entered is not valid) ');
-      	} else {
-      	  //If successful in fetching HTML from normalized URL, send ID in response
-      	  res.status(200).send(`'${data.insertId}'`);
-      	  //Add HTML to Redis Cache
-      	  client.set(`'${data.insertId}'`, escape(body), redis.print);
-      	  //Then add the HTML to the database
-      	  database.updateHtml(escape(body), data.insertId, (fault, results) => {
-      	  	if(fault) {
-      	  	  console.log('error:', fault);
-      	  	};
-      	  });  		
-      	};
-      });
-     };
+  //Create a job within the queue
+  const job = queue.create('url', {
+      title: url
+  })
+  job.on('complete', function(){
+      if (res.finished) {
+          console.log("Job complete");
+      } else {
+          res.sendStatus(200).send(`${job.id}`);
+      }
+  }).on('failed', function(){
+      if (res.finished) {
+          console.log("Job failed");
+      } else {
+          res.send(" Not Available (The URL you entered is not valid) ");
+      }
+  }).on('progress', function(progress){
+      console.log('job #' + job.id + ' ' + progress + '% complete');
   });
+    
+  job.save();
+
+  //Processes up to 5 jobs concurrently, calling done when finished
+  queue.process('url', 5, (job, done) => {
+      //Enters URL into database
+    database.findID(url, (err, data) => {
+      if(err) {
+        done(err);
+      } else {
+        //If no error in entering the URL into the database, request for the HTML of the page
+        //using the normalized version of the URL
+        request(normalizeUrl(url), (error, response, body) => {
+          if(error) {
+            //If error in fetching HTML from normalized URL, send response "Not Available"
+            done(error);
+          } else {
+            //If successful in fetching HTML from normalized URL, send ID in response
+            done();
+            //Then add the HTML to the database
+            database.updateHtml(escape(body), data.insertId, (fault, results) => {
+              if(fault) {
+                console.log('Error:', fault);
+              };
+            });     
+          };
+        });
+       };
+    });
+  });
+
 });
 
 //Handles get requests from client with JobID submission
